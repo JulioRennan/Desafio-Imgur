2676c460b9ff40a7e54f5f33ffb223994f24c2d5
Julio Rennan <julio.rennann@gmail.com>
2022-02-19 00:48:38 -0400

    Initial Commit

---
+++ DesafioImgur/DesafioImgurApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioImgurApp.swift
+//  DesafioImgur
+//
+//  Created by Julio Rennan on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DesafioImgurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
